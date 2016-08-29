8cbd58470d9e2fb45791dd286f3f7a9fae21b782
Colin Aardsma <colinaardsma@Colins-Mac-mini.local>
2016-08-29 16:32:14 -0500

    mostly working ascii art page

--- asciichan.py
+++ asciichan.py
@@ -5,7 +5,8 @@ import jinja2
 from google.appengine.ext import db
 
 template_dir = os.path.join(os.path.dirname(__file__), 'templates')
-jinja_env = jinja2.Environment(loader = jinja2.FileSystemLoader(template_dir), autoescape = True)
+jinja_env = jinja2.Environment(loader = jinja2.FileSystemLoader(template_dir),
+                               autoescape = True)
 
 class Handler(webapp2.RequestHandler):
     def write(self, *a, **kw):
@@ -18,9 +19,30 @@ class Handler(webapp2.RequestHandler):
     def render(self, template, **kw):
         self.write(self.render_str(template, **kw))
 
+class Art(db.Model):
+    title = db.StringProperty(required = True) #sets title to a string and makes it required
+    art = db.TextProperty(required = True) #sets title to a text and makes it required (text is same as string but can be more than 500 characters and cannot be indexed)
+    created = db.DateTimeProperty(auto_now_add = True) #sets created to equal current date/time
+
 class MainPage(Handler):
+    def render_front(self, title="", art="", error="", arts=""):
+        arts = db.GqlQuery("SELECT * FROM Art ORDER BY created DESC") #table is named Art because class is named Art (the class creates the table)
+        self.render("front.html", title=title, art=art, error=error, arts=arts)
+
     def get(self):
-        self.write("asciichan!")
+        self.render_front()
+
+    def post(self):
+        title = self.request.get("title")
+        art = self.request.get("art")
+
+        if title and art:
+            a = Art(title = title, art = art) #creates new art object named a
+            a.put() #stores a in database
+            self.redirect("/") #sends you back to main page
+        else:
+            error = "we need both a title and some artwork!"
+            self.render_front(title, art, error)
 
 app = webapp2.WSGIApplication([
     ('/', MainPage)
