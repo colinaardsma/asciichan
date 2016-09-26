a2cb2a87c7e3fbcdbb31a18f41a344f9e1d769f3
Colin Aardsma <colinaardsma@Colins-Mac-mini.local>
2016-09-26 16:27:10 -0500

    retrieving ip, translating ip to coords, generating google maps api url; need to turn url into picture

--- asciichan.py
+++ asciichan.py
@@ -18,37 +18,48 @@ class Handler(webapp2.RequestHandler):
     def render(self, template, **kw):
         self.write(self.render_str(template, **kw))
 
-IP_URL = "http://api.hostip.info/?ip="
-def get_coords(ip):
+IP_URL = "http://freegeoip.net/xml/"
+def get_coords(ip): #determines coordinates based on IP provided (doesn't work when running locally)
+    ip = "4.2.2.2"
     url = IP_URL + ip
     content = None
     try:
-        content = urllin2.urlopen(url).read()
-    except URLError:
+        content = urllib2.urlopen(url).read()
+    except urllib2.URLError:
         return
 
     if content:
         #parse xml and find coordinates
-        p = minidom.parseString(xml)
-        coords = p.getElementsByTagName('gml:coordinates')
-        if coords and coords[0].childNodes[0].nodeValue:
-            lon = coords[0].childNodes[0].nodeValue.split(",")[0]
-            lat = coords[0].childNodes[0].nodeValue.split(",")[1]
-            loc = lat + ", " + lon
-        print lat, lon
-        return lat, lon
+        p = minidom.parseString(content)
+        lat = p.getElementsByTagName('Latitude')[0].childNodes[0].nodeValue
+        lon = p.getElementsByTagName('Longitude')[0].childNodes[0].nodeValue
+        return db.GeoPt(lat, lon)
+
 
 class Art(db.Model):
     title = db.StringProperty(required = True) #sets title to a string and makes it required
     art = db.TextProperty(required = True) #sets title to a text and makes it required (text is same as string but can be more than 500 characters and cannot be indexed)
     created = db.DateTimeProperty(auto_now_add = True) #sets created to equal current date/time
+    coords = db.GeoPtProperty() #if you make something
 
 class MainPage(Handler):
     def render_front(self, title="", art="", error="", arts=""):
         arts = db.GqlQuery("SELECT * FROM Art ORDER BY created DESC") #table is named Art because class is named Art (the class creates the table)
-        self.render("front.html", title=title, art=art, error=error, arts=arts)
+        arts = list(arts) #turns the db query above into a list object so that it doesnt run a new db search each time it is called
+
+        mapUrl = "https://maps.googleapis.com/maps/api/staticmap?size=600x300&maptype=roadmap"
+
+        for art in arts: #find which arts have coords
+            if art.coords:  #if we have any arts coords, make an image url
+                marker = "&markers=" + str(art.coords)
+                mapUrl += marker
+
+        #dsiplay the image url
+        self.render("front.html", title=title, art=art, error=error, arts=arts, mapUrl=mapUrl)
 
     def get(self):
+        self.write(self.request.remote_addr)
+        self.write(repr(get_coords(self.request.remote_addr))) #displays coords for reqeusting IP (repr removes <> from python value so it will print correctly in html)
         self.render_front()
 
     def post(self):
@@ -58,6 +69,10 @@ class MainPage(Handler):
         if title and art:
             a = Art(title = title, art = art) #creates new art object named a
             #lookup user's coordinates from IP
+            coords = get_coords(self.request.remote_addr)
+            if coords:
+                a.coords = coords
+            #if we have coordinates, add them to the Art
 
             a.put() #stores a in database
             self.redirect("/") #sends you back to main page
